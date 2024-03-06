3ac94e73b980604c11f649dd93d659f65deca10b
Hassan Ashraf <hassan.ashraf@Hassan-Ashraf---MAC.local>
2024-03-06 13:22:43 +0200

    Initial Project

---
+++ cicd-iosUITests/cicd_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  cicd_iosUITestsLaunchTests.swift
+//  cicd-iosUITests
+//
+//  Created by Hassan Ashraf on 06/03/2024.
+//
+
+import XCTest
+
+final class cicd_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
